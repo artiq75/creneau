25fe5e46a42e6810798d9bc5eeb93e83dd2d63d5
artiq75 <artiq75@gmail.com>
2022-03-31 14:54:56 +0200

    Formattage des creneaux

--- src/crenel.rs
+++ src/crenel.rs
@@ -1,18 +1,18 @@
 use crate::user;
 
-pub fn get_crenel() -> [i8; 2] {
-    println!("Debut: ");
-    let begin = loop {
+fn get_begin() -> i8 {
+    loop {
         let value = user::get_i8_input();
         if value < 0 || value > 23 {
             println!("Le nombre doit être >= 0 ou <= 23");
             continue;
         }
         break value;
-    };
+    }
+}
 
-    println!("Fin: ");
-    let end = loop {
+fn get_end(begin: i8) -> i8 {
+    loop {
         let value = user::get_i8_input();
         if value < 0 || value > 23 {
             println!("Le nombre doit être >= 0 ou <= 23");
@@ -23,7 +23,15 @@ pub fn get_crenel() -> [i8; 2] {
             continue;
         }
         break value;
-    };
+    }
+}
+
+pub fn get_crenel() -> [i8; 2] {
+    println!("Debut: ");
+    let begin = get_begin();
+
+    println!("Fin: ");
+    let end = get_end(begin);
 
     [begin, end]
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ mod user;
 mod crenel;
 
 fn main() {
+    let mut formatted_crenels: Vec<String> = Vec::new();
+    
     let crenels = crenel::get_crenels();
-    dbg!(crenels);
+    for [begin, end] in &crenels {
+        formatted_crenels.push(format!("de {}h à {}h", begin, end));
+    }
+    
+    println!("Le magasin ouvre: {}", formatted_crenels.join(" et "));
 }
